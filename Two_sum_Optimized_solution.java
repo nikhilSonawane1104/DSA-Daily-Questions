8da0b37c844cd3ed9f4d876878086c3e7ec83c36
Nikhil Balasaheb Sonawane <86091935+nikhilSonawane1104@users.noreply.github.com>
2023-09-27 15:07:13 +0530

    Create Two_sum_Optimized_solution.java

---
+++ Two_sum_Optimized_solution.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+public class Solution {
+    public static String read(int n, int []book, int target){
+
+ 
+     Arrays.sort(book);
+     int left=0;
+     int right=book.length-1;
+     while(left<right)
+     {
+         int sum=book[left]+book[right];
+         if(sum==target)
+         {
+             return "YES";
+         }
+        else if(sum<target)
+         {
+             left++;
+         }
+         else{
+             right--;
+         }
+     
+     }
+
+     return "NO";
+    } 
+
+}
