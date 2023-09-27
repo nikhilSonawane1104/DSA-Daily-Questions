c994625911443b900173ae11e926aef2211138b5
Nikhil Balasaheb Sonawane <86091935+nikhilSonawane1104@users.noreply.github.com>
2023-09-27 16:17:00 +0530

    Create majority_mores_voting_algorithms.java

---
+++ majority_mores_voting_algorithms.java
@@ -0,0 +1,40 @@
+public class Solution {
+    public static int majorityElement(int []v) {
+        // Write your code here
+
+        int cnt=0;
+        int el=0;
+        for(int i=0;i<v.length;i++)
+        {
+            if(cnt==0)
+            {
+                cnt=1;
+                el=v[i];
+            }
+            else if(v[i]==el)
+            {
+                cnt++;
+            }
+            else
+            {
+                cnt--;
+
+            }
+        }
+        int cnt1=0;
+        for(int i=0;i<v.length;i++)
+        {
+            if(v[i]==el)
+            {
+                cnt1++;
+            } 
+        }
+
+        if(cnt1>v.length/2)
+        {
+            return el;
+        }
+
+        return -1;
+    }
+}
