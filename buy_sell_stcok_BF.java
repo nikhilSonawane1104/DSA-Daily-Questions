75a9a2decde4d447bacc3278dd71742ab71bd497
Nikhil Balasaheb Sonawane <86091935+nikhilSonawane1104@users.noreply.github.com>
2023-09-28 14:15:53 +0530

    Create buy_sell_stcok_BF.java

---
+++ buy_sell_stcok_BF.java
@@ -0,0 +1,14 @@
+// Stock Buy And Sell 
+public class Solution {
+    public static int bestTimeToBuyAndSellStock(int []arr) {
+       int maxPro = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] > arr[i]) {
+                    maxPro = Math.max(arr[j] - arr[i], maxPro);
+                }
+            }
+        }
+        return maxPro;
+    }
+}
